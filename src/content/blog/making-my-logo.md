dc54692fdeda0173a87a9a79c94e72ef89ba7bbc
flatlit <258610908+flatlit@users.noreply.github.com>
2026-03-06 10:22:54 +0000

    add rss feed link with only published

---
+++ src/content/blog/making-my-logo.md
@@ -0,0 +1,7 @@
+---
+title: "Making the Flat Lit Studio logo"
+description: "An example of my art process"
+pubDate: "01 Apr 2026"
+---
+
+This is a bit of a block bench tutorial
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,9 +1,10 @@
 import { getCollection } from "astro:content";
 import rss from "@astrojs/rss";
 import { SITE_DESCRIPTION, SITE_TITLE } from "../consts";
+import { publishedFilter } from "../utils";
 
 export async function GET(context) {
-  const posts = await getCollection("blog");
+  const posts = await getCollection("blog", publishedFilter);
   return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
